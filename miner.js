93fa9504cdcdac86f279e44dbb28352aeab8fd54
Germano <gmergel@gmail.com>
2013-05-31 10:46:46 -0300

    Update miner.js

--- miner.js
+++ miner.js
@@ -21,6 +21,8 @@ Array.prototype.unique = function () {
         $this.STYLES = {
             BOXWRAPPER: '#boxWrapper{width: 100%;position: absolute;top: 0;text-align: center;font-family:Trebuchet MS; font-size: 13px}',
             BOX: '#box{width: 70%;background-color: white;height: 500px;padding-top: 20px;margin: 70px auto;text-align: center;border-radius: 6px;box-shadow: 2px 2px 12px #ddd, -2px -2px 12px #ddd;}',
+            DOC: '.doc{border-radius: 4px;background-color: white;box-shadow: 0px 5px 0px 2px #F2F2F2;color: #666;line-height: 23px;width: 100px;height: 120px;border: 1px solid #eee;display: inline-block;margin: 20px 30px auto;text-align: center;overflow: hidden;}',
+            DOCSBOX: '#docsBox{height: 200px;background-color: #fafafa;margin-top: 20px;box-shadow: 0px 3px 6px -1px #f2f2f2 inset;border-top: 1px solid #eee;}',
             WORDS: '.queryWord{background-color: #bbdaf7;padding: 7px;color: white;border-radius: 5px;margin-right: 10px;}',
             ACTIVEWORD: '.queryWord.active{background-color:#79bcff};'
         };
@@ -28,21 +30,28 @@ Array.prototype.unique = function () {
         $this.init = function () {
             //include CSS:
             var styles = document.createElement('style');
+            
+            for(var style in $this.STYLES){
+                styles.innerHTML += $this.STYLES[style];
+            }
+            /*
             styles.innerHTML = $this.STYLES.BOXWRAPPER +
                                $this.STYLES.BOX +
+                               $this.STYLES.DOCSBOX +
                                $this.STYLES.WORDS+
                                $this.STYLES.ACTIVEWORD;
-                                                  
+              */                                    
             $("head").append(styles);
 
             //blur page
             $('#LayoutWrapper').css('opacity', 0.15);
             
+            //get abstracts
+            $this.getAbs();
             //prepare box layout elements
             $this.getQuery();
             $this.createBox();
-            //calculates tf-idf
-            $this.getAbs();
+            //calc
             $this.countWords(0);
             $this.allWords();
 
@@ -53,6 +62,7 @@ Array.prototype.unique = function () {
             $(".abstract").each(function (key) {
                 var words = [];
                 $this.abs[key] = $(this);
+                $this.abs[key].title = $($this.abs[key]).parents('.detail').find('h3').text().trim();
                 $this.abs[key].txt = $this.abs[key].text().replace(/^[\s]/g, '').replace(/\s+/g, ' ').toLowerCase();
                 words = $this.abs[key].txt.split(/\s|,|\.|-|Â»|\/|\\|\(|\)|:|'s/ig).filter(function (value) { return value !== "" && value !== null; });
                 $this.abs[key].words = words;
@@ -94,13 +104,21 @@ Array.prototype.unique = function () {
                 queryBox.appendChild(newSpan);
             });
 
-            //queryBox.innerHTML = '<span class="queryWord" id="word1">text</span><span class="queryWord" id="word2">mining</span>';
             $this.box.appendChild(queryBox);
 
             //docsBox
             var docsBox = document.createElement('div');
             docsBox.id = 'docsBox';
-            docsBox.innerHTML = '';
+            
+            $(MINER.abs).each(function(key,value){
+                var newSpan = document.createElement('span');
+                newSpan.id = 'doc'+key;
+                newSpan.className = 'doc';
+                console.log(value.title);
+                newSpan.innerHTML = value.title;
+                docsBox.appendChild(newSpan);
+            });
+            
             $this.box.appendChild(docsBox);
 
             $(wrapper).append($this.box);
