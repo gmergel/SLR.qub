fb3513a58556b118b084d14b0697daeb683d888d
Germano <gmergel@gmail.com>
2013-06-07 11:26:40 -0300

    Update miner.js

--- miner.js
+++ miner.js
@@ -8,13 +8,23 @@ Array.prototype.unique = function () {
     return r;
 };
 
+var stemmer = function () {
+    function h() { } function i() { console.log(Array.prototype.slice.call(arguments).join(" ")) } var j = { ational: "ate", tional: "tion", enci: "ence", anci: "ance", izer: "ize", bli: "ble", alli: "al", entli: "ent", eli: "e", ousli: "ous", ization: "ize", ation: "ate", ator: "ate", alism: "al", iveness: "ive", fulness: "ful", ousness: "ous", aliti: "al", iviti: "ive", biliti: "ble", logi: "log" }, k = { icate: "ic", ative: "", alize: "al", iciti: "ic", ical: "ic", ful: "", ness: "" }; return function (a, l) {
+        var d, b, g, c, f, e; e = l ? i : h; if (3 > a.length) return a;
+        g = a.substr(0, 1); "y" == g && (a = g.toUpperCase() + a.substr(1)); c = /^(.+?)(ss|i)es$/; b = /^(.+?)([^s])s$/; c.test(a) ? (a = a.replace(c, "$1$2"), e("1a", c, a)) : b.test(a) && (a = a.replace(b, "$1$2"), e("1a", b, a)); c = /^(.+?)eed$/; b = /^(.+?)(ed|ing)$/; c.test(a) ? (b = c.exec(a), c = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*/, c.test(b[1]) && (c = /.$/, a = a.replace(c, ""), e("1b", c, a))) : b.test(a) && (b = b.exec(a), d = b[1], b = /^([^aeiou][^aeiouy]*)?[aeiouy]/, b.test(d) && (a = d, e("1b", b, a), b = /(at|bl|iz)$/, f = /([^aeiouylsz])\1$/, d = /^[^aeiou][^aeiouy]*[aeiouy][^aeiouwxy]$/,
+        b.test(a) ? (a += "e", e("1b", b, a)) : f.test(a) ? (c = /.$/, a = a.replace(c, ""), e("1b", f, a)) : d.test(a) && (a += "e", e("1b", d, a)))); c = /^(.*[aeiouy].*)y$/; c.test(a) && (b = c.exec(a), d = b[1], a = d + "i", e("1c", c, a)); c = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/; c.test(a) && (b = c.exec(a), d = b[1], b = b[2], c = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*/, c.test(d) && (a = d + j[b], e("2", c, a))); c = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
+        c.test(a) && (b = c.exec(a), d = b[1], b = b[2], c = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*/, c.test(d) && (a = d + k[b], e("3", c, a))); c = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/; b = /^(.+?)(s|t)(ion)$/; c.test(a) ? (b = c.exec(a), d = b[1], c = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*[aeiouy][aeiou]*[^aeiou][^aeiouy]*/, c.test(d) && (a = d, e("4", c, a))) : b.test(a) && (b = b.exec(a), d = b[1] + b[2], b = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*[aeiouy][aeiou]*[^aeiou][^aeiouy]*/,
+        b.test(d) && (a = d, e("4", b, a))); c = /^(.+?)e$/; if (c.test(a) && (b = c.exec(a), d = b[1], c = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*[aeiouy][aeiou]*[^aeiou][^aeiouy]*/, b = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*([aeiouy][aeiou]*)?$/, f = /^[^aeiou][^aeiouy]*[aeiouy][^aeiouwxy]$/, c.test(d) || b.test(d) && !f.test(d))) a = d, e("5", c, b, f, a); c = /ll$/; b = /^([^aeiou][^aeiouy]*)?[aeiouy][aeiou]*[^aeiou][^aeiouy]*[aeiouy][aeiou]*[^aeiou][^aeiouy]*/; c.test(a) && b.test(a) && (c = /.$/, a = a.replace(c, ""), e("5",
+        c, b, a)); "y" == g && (a = g.toLowerCase() + a.substr(1)); return a
+    }
+}();
+
 (function ($) {
     (function () {
 
         var $this = this;
         $this.abs = [];
-        $this.words = [];
-        $this.allwords = [];
+        $this.df = new Array();
 
         $this.step = 160;
         $this.moveby = 2;
@@ -25,8 +35,8 @@ Array.prototype.unique = function () {
             BOXWRAPPER: '#boxWrapper{width: 100%;position: absolute;top: 0;text-align: center;font-family:Trebuchet MS; font-size: 13px}',
             BOX: '#box{width: 70%;background-color: white;height: 500px;padding-top: 20px;margin: 70px auto;text-align: center;border-radius: 6px;box-shadow: 2px 2px 12px #ddd, -2px -2px 12px #ddd;}',
             DOCSLI: '#docsBox li{display: inline-block;margin: 20px 30px auto;text-align: center;width: 100px;vertical-align: top;}',
-            DOC: '.doc:hover{cursor:pointer;border: 1px solid red}.doc{display: block;border-radius: 4px;background-color: white;box-shadow: 0px 5px 0px 2px #F2F2F2;color: #666;line-height: 23px;height: 120px;border: 1px solid #eee;overflow: hidden;}',
-            DOCSBOX: '#docsBox{-webkit-transition: margin-left 0.8s ease-out;height: 200px;width: 9000px;text-align: left;padding: 0;margin: 0 0 0 64px;}',
+            DOC: '.doc:hover{cursor:pointer;border: 1px solid red}.doc{display: block;border-radius: 4px;background-color: white;box-shadow: 0px 2px 2px 0px #F2F2F2;color: #666;line-height: 23px;height: 120px;border: 1px solid #eee;overflow: hidden;}',
+            DOCSBOX: '#docsBox{-webkit-transition: margin-left 0.8s ease-out;height: 280px;width: 99000px;text-align: left;padding: 0;margin: 0 0 0 64px;}',
             DOCSWRAPPER: '#docsWrapper{overflow: hidden;margin: 20px 0;padding: 0;box-shadow: 0px 3px 6px -1px #f2f2f2 inset;border-top: 1px solid #eee;}',
             WORDS: '.queryWord{background-color: #bbdaf7;padding: 7px;color: white;border-radius: 5px;margin-right: 10px;}',
             BUTTONS: '.btn{background-color: green;padding: 5px;border-radius: 6px;height: 100px;color: white;line-height: 36px;margin-right: 35px;box-shadow: 1px 1px 0px 0px #ccc;}.btn:active{box-shadow: 0 0}',
@@ -59,11 +69,14 @@ Array.prototype.unique = function () {
 
             //get abstracts
             $this.getAbs();
+            //count words
+            $this.countWords();
+            $this.TfIdf();
             //prepare box layout elements
             $this.getQuery();
             $this.createBox();
             //calc
-            $this.allWords();
+            //$this.allWords();
 
             console.warn("done");
         }
@@ -75,6 +88,8 @@ Array.prototype.unique = function () {
                 $this.abs[key].title = $($this.abs[key]).parents('.detail').find('h3').text().trim();
                 $this.abs[key].txt = $this.abs[key].text().replace(/^[\s]/g, '').replace(/\s+/g, ' ').toLowerCase();
                 words = $this.abs[key].txt.split(/\s|,|\.|-|Â»|\/|\\|\(|\)|:|'s/ig).filter(function (value) { return value !== "" && value !== null; });
+                for (var wordkey in words)
+                    if (words.hasOwnProperty(wordkey)) words[wordkey] = stemmer(words[wordkey]);
                 $this.abs[key].words = words;
                 $this.abs[key].uniqueWords = words.unique().sort();
             });
@@ -85,19 +100,19 @@ Array.prototype.unique = function () {
             $($this.abs).each(function () {
                 
                 var words = this.words;
-                console.log(words);
                 var positions = new Array();
                 var word_counts = new Array();
 
                 for (var i = 0; i < words.length; i++) {
                     
                     var word = " " + words[i];
-                    
                     if (!word) continue;
                     if (typeof positions[word] == 'undefined') {
                         positions[word] = word_counts.length;
                         word_counts.push([word, 1]);
+                        ($this.df[word]) ? $this.df[word]++ : $this.df[word] = 1;
                     } else word_counts[positions[word]][1]++;
+
                 }
                 // Put most frequent words at the beginning.
                 word_counts.sort(function (a, b) { return b[1] - a[1] })
@@ -107,11 +122,28 @@ Array.prototype.unique = function () {
 
         }
 
-        $this.allWords = function () {
-            $($this.abs).each(function (key, item) {
-                $this.allwords = $this.allwords.concat(this.words);
+        $this.TfIdf = function () {
+            var positions = new Array();
+            $($this.abs).each(function (key, value) {
+                value.tfidf = new Array();
+                for (var wordkey in value.tf) {
+                    if (value.tf.hasOwnProperty(wordkey) && value.tf[wordkey]) {
+                        var word =  value.tf[wordkey][0];
+                        var termf = value.tf[wordkey][1];
+                        var df = MINER.df[word];
+                        //word[0];
+                        
+                        //if(typeof positions[word] == 'undefined')
+
+                        if (typeof value.tfidf[wordkey] == 'undefined') value.tfidf[wordkey] = [word, termf / df];
+                        //value.tfidf[wordkey][0] = word;
+                        //value.tfidf[wordkey][1] = termf / df;
+                        
+                        //console.log(word + " = " + termf / df);
+                    }
+                }
+                value.tfidf.sort(function (a, b) { return b[1] - a[1] })
             });
-            $this.allwords = $this.allwords.unique().sort();
         }
 
         $this.createBox = function () {
@@ -149,18 +181,27 @@ Array.prototype.unique = function () {
 
                 var newId = document.createElement('div');
                 newId.id = 'docref';
-                newId.innerHTML = '#' + key;
+                newId.innerHTML = '#' + (key+1);
 
                 var newSpan = document.createElement('span');
                 newSpan.id = 'doc' + key;
                 newSpan.className = 'doc';
                 newSpan.innerHTML = value.title;
+                
+                var newFWordsSpan = document.createElement('span');
+                newFWordsSpan.id = 'fwords' + key;
+                newFWordsSpan.className = 'fwords';
 
+                for (var i = 0; i < 5; i++) {
+                    newFWordsSpan.innerHTML += value.tfidf[i][0].trim()+'</br>';
+                }
+                
                 var newWordList = document.createElement('div');
                 newWordList.id = 'wl' + key;
 
                 newLi.appendChild(newId);
                 newLi.appendChild(newSpan);
+                newLi.appendChild(newFWordsSpan);
                 docsBox.appendChild(newLi);
             });
 
